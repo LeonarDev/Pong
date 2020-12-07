ac51f709511c66ed1b3a96aa2139eaf3813a3ff7
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-07 12:07:27 -0300

    Starting repository and creating ball

---
+++ script.js
@@ -0,0 +1,13 @@
+let xAxisBall = 300;
+let yAxisBall = 200;
+let diameterBall = 15;
+let radiusBall = diameterBall / 2;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(40);
+  createBall();
+}
